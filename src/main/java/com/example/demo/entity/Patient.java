fb58b541ed336d812708518da138f0e9f947ae3d
Rudra Rajawat <rudrarajawat001@gmail.com>
2025-01-23 16:09:45 +0530

    created entity classes

---
+++ src/main/java/com/example/demo/entity/Patient.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="Patient_Table")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pId;
+	private String pName;
+	private String pAdderess;
+	private String pGender;
+}
